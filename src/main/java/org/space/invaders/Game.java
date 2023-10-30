a0b4e203a93409ff55b9a040dd3cfed6c3093b25
JoseCostaUni <up202207871@up.pt>
2023-10-30 18:03:17 +0000

    Implemented some functions

---
+++ src/main/java/org/space/invaders/Game.java
@@ -0,0 +1,44 @@
+package org.space.invaders;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import javax.swing.*;
+public class Game {
+    private Screen screen;
+    private Arena arena;
+    public Game(){
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        arena = new Arena(40, 20);
+    }
+
+    private void draw() throws IOException{
+        screen.clear();
+        arena.draw(screen.newTextGraphics());
+        screen.refresh();
+    }
+}

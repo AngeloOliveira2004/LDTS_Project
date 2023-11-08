a40aee7583833831e4f4eb1fdb96db503ae9f07e
JoseCostaUni <up202207871@up.pt>
2023-11-08 19:14:36 +0000

    altered shot a little

--- src/main/java/org/spaceInvaders/Shot.java
+++ src/main/java/org/spaceInvaders/Shot.java
@@ -6,22 +6,33 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class Shot {
 
-    private int yVelocity = -2;
+    private int yVelocity = -1;
     Position position;
+    private boolean update = true;
     public Shot(int x , int y) {
         this.position = new Position(x , y);
     }
-
     public void draw(TextGraphics screen)
     {
         while(position.getY() > 0)
         {
-            position.setY(position.getY()+yVelocity);
+            if(update)
+            {
+                TextCharacter character = new TextCharacter('|');
+                character = character.withBackgroundColor(TextColor.ANSI.BLACK);
+                character = character.withForegroundColor(TextColor.ANSI.YELLOW);
+                screen.setCharacter(position.getX(), position.getY() - 1, character);
+                update = false;
+            }
+            position.setY((int) (position.getY()+yVelocity));
+            if(position.getY() % 5  == 0)
+            {
+                TextCharacter character = new TextCharacter('|');
+                character = character.withBackgroundColor(TextColor.ANSI.BLACK);
+                character = character.withForegroundColor(TextColor.ANSI.YELLOW);
+                screen.setCharacter(position.getX(), position.getY(), character);
+            }
 
-            TextCharacter character = new TextCharacter('|');
-            character = character.withBackgroundColor(TextColor.ANSI.BLACK);
-            character = character.withForegroundColor(TextColor.ANSI.YELLOW);
-            screen.setCharacter(position.getX(), position.getY(), character);
         }
     }
 }

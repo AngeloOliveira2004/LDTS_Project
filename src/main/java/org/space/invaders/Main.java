32c8b86196ad9d0b5f38fedc7427e7a2cf873f08
JoseCostaUni <up202207871@up.pt>
2023-11-24 19:33:39 +0000

    New structure according to UML

--- src/main/java/org/space/invaders/Main.java
+++ src/main/java/org/space/invaders/Main.java
@@ -4,11 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game();
         game.run();
-        String imagePath = "/home/jose-costa/Downloads/p.jpg";
-
-         //Set the desired output width for the ASCII art
-        int outputWidth = 75;
-        int outputHeight = 30;
 
     }
 }

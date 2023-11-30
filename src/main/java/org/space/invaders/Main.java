10fb83ac735229cecfbac89370ce9cda20a94edf
JoseCostaUni <up202207871@up.pt>
2023-11-30 16:54:24 +0000

    Testing somethings

--- src/main/java/org/space/invaders/Main.java
+++ src/main/java/org/space/invaders/Main.java
@@ -4,7 +4,7 @@ import org.space.invaders.model.game.menu.Menu;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TestClass testClass = new TestClass(100 , 50);
+        TestClass testClass = new TestClass(200 , 100);
         //Menu menu = new Menu();
     }
 }
--- src/main/java/org/space/invaders/control/Controller.java
+++ src/main/java/org/space/invaders/control/Controller.java
@@ -1,6 +1,6 @@
 package org.space.invaders.control;
 
-public interface Controller{
-    public abstract void processKey(boolean menu , com.googlecode.lanterna.input.KeyStroke key);
+public class Controller{
+
 
 }

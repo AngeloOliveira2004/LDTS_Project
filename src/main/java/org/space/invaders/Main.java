dc7be4fd34521582be4b60623188e01818358183
JoseCostaUni <up202207871@up.pt>
2023-11-30 14:12:09 +0000

    Doing some experiments

--- src/main/java/org/space/invaders/Main.java
+++ src/main/java/org/space/invaders/Main.java
@@ -3,7 +3,8 @@ package org.space.invaders;
 import org.space.invaders.model.game.menu.Menu;
 
 public class Main {
-    public static void main(String[] args) {
-        Menu menu = new Menu();
+    public static void main(String[] args) throws Exception {
+        TestClass testClass = new TestClass(100 , 50);
+        //Menu menu = new Menu();
     }
 }

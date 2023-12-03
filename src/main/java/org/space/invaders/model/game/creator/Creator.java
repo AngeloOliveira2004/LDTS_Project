a34195fd685521ee4889735ce2040c8304e21492
JoseCostaUni <up202207871@up.pt>
2023-12-03 15:47:49 +0000

    Started Creators

---
+++ src/main/java/org/space/invaders/model/game/creator/Creator.java
@@ -0,0 +1,7 @@
+package org.space.invaders.model.game.creator;
+
+import org.space.invaders.model.game.elements.Element;
+
+public abstract class Creator {
+    public abstract Element create();
+}
---
+++ src/main/java/org/space/invaders/model/game/creator/MiniSpaceshipCreator.java
@@ -0,0 +1,4 @@
+package org.space.invaders.model.game.creator;
+
+public class MiniSpaceshipCreator {
+}

d0762157b6ea59d66bc6a91b0391bbbcc09e1c2f
AngeloOliveira2004 <angeloaraujo2004@gmail.com>
2023-12-04 18:57:04 +0000

    Working on Leaderboard Menu

--- src/main/java/org/space/invaders/view/menu/LeaderboardView.java
+++ src/main/java/org/space/invaders/view/menu/LeaderboardView.java
@@ -1,4 +1,30 @@
 package org.space.invaders.view.menu;
 
-public class LeaderboardView {
+import com.googlecode.lanterna.screen.Screen;
+import org.space.invaders.gui.MenuGUI;
+import org.space.invaders.model.Position;
+import org.space.invaders.model.game.menu.MenuModel;
+
+public class LeaderboardView extends MenuView {
+    public LeaderboardView(MenuModel model, Screen screen) {
+        super(model, screen);
+    }
+
+    public void drawElements(MenuGUI gui) {
+        MenuModel menu = (MenuModel) getModel();
+        gui.drawText(new Position(15,8),"I N S T R U C T I O N S","#008000","BLINK");
+        String isSelected = menu.getCurrentOption();
+
+        for (int i = 0; i < menu.getNumberOptions(); i++) {
+
+            if (isSelected.equals(menu.getOption(i))) {
+                gui.drawSelectedText(new Position(22, 10 + i * 2), menu.getOption(i), "#008000", "BLINK");
+            } else {
+                gui.drawText(new Position(22, 10 + i * 2), menu.getOption(i), "#008000", "BOLD");
+            }
+            menu.setCurrentOption();
+        }
+
+        gui.refresh();
+    }
 }

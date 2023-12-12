0652a1ed87191cabb0b7d437fe8ef6cd5975cb00
JoseCostaUni <up202207871@up.pt>
2023-12-12 23:21:06 +0000

    Merge complete i think

--- src/main/java/org/space/invaders/model/game/creator/ShotFactory.java
+++ src/main/java/org/space/invaders/model/game/creator/ShotFactory.java
@@ -1,15 +1,20 @@
 package org.space.invaders.model.game.creator;
 
+import org.space.invaders.model.Position;
+import org.space.invaders.model.game.elements.MiniShot;
+import org.space.invaders.model.game.elements.Shot;
 import org.space.invaders.model.game.elements.ShotElement;
 import org.space.invaders.model.game.elements.SuperShot;
 
 public class ShotFactory {
-    public ShotElement createShot() {
-        return null;
+    public ShotElement createShot(Position position) {
+        Position position_ = new Position(position.x + 8 , position.y - 4);
+        return new Shot(position_ , 1 , 5);
     }
-    public ShotElement createMiniShot()
+    public ShotElement createMiniShot(Position position)
     {
-        return null;
+        Position position_ = new Position(position.x + 3 , position.y - 4);
+        return new MiniShot(position_, 1);
     }
     public ShotElement createTrackingShot()
     {

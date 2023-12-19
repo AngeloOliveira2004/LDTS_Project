f29806768825992af80c1e0d8dc62029f69f490d
AngeloOliveira2004 <angeloaraujo2004@gmail.com>
2023-12-19 12:39:33 +0000

    Added Default Enemy

--- src/test/java/org/space/invaders/model/game/elements/DefaultEnemyTest.java
+++ src/test/java/org/space/invaders/model/game/elements/DefaultEnemyTest.java
@@ -3,14 +3,10 @@ package org.space.invaders.model.game.elements;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.space.invaders.view.game.DefaultEnemyView;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.*;
@@ -45,5 +41,27 @@ class DefaultEnemyTest {
         defaultEnemy.setDefaultEnemyView(newEnemyView);
 
         assertSame(newEnemyView, defaultEnemy.getDefaultEnemyView());
+    }
+
+    @Test
+    void testGetDefaultEnemyView() {
+        DefaultEnemyView defaultEnemyViewMock = mock(DefaultEnemyView.class);
+        defaultEnemy.setDefaultEnemyView(defaultEnemyViewMock);
+
+        assertEquals(defaultEnemyViewMock, defaultEnemy.getDefaultEnemyView());
+    }
+
+    @Test
+    void testDraw() {
+        TextGraphics textGraphics = mock(TextGraphics.class);
+
+        DefaultEnemy defaultEnemy = new DefaultEnemy(10, 5, 1, 1, 100, 1, true, 1, 1);
+
+        DefaultEnemyView defaultEnemyViewMock = mock(DefaultEnemyView.class);
+        defaultEnemy.setDefaultEnemyView(defaultEnemyViewMock);
+
+        defaultEnemy.draw(textGraphics);
+
+        Mockito.verify(defaultEnemyViewMock,Mockito.times(0)).drawDefaultEnemy();
     }
 }
